6b06607e675396e624a10374e6da620e87174304
Kelly Davis <kdavis@mozilla.com>
2015-08-10 09:50:19 +0200

    Added scripts to make dirs and preprocess all supported languages

---
+++ make_directories.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+mkdir -p language-models/pre-processed/as
+mkdir -p language-models/pre-processed/ca
+mkdir -p language-models/pre-processed/cs
+mkdir -p language-models/pre-processed/cy
+mkdir -p language-models/pre-processed/de
+mkdir -p language-models/pre-processed/en
+mkdir -p language-models/pre-processed/en-gb
+mkdir -p language-models/pre-processed/en-za
+mkdir -p language-models/pre-processed/es
+mkdir -p language-models/pre-processed/es-ar
+mkdir -p language-models/pre-processed/es-cl
+mkdir -p language-models/pre-processed/es-es
+mkdir -p language-models/pre-processed/es-mx
+mkdir -p language-models/pre-processed/fr
+mkdir -p language-models/pre-processed/gl
+mkdir -p language-models/pre-processed/hr
+mkdir -p language-models/pre-processed/it
+mkdir -p language-models/pre-processed/nb-no
+mkdir -p language-models/pre-processed/pt-br
+mkdir -p language-models/pre-processed/pt-pt
+mkdir -p language-models/pre-processed/ru
+mkdir -p language-models/pre-processed/sl
---
+++ preprocess.sh
@@ -0,0 +1,23 @@
+#!/bin/bash &
+./bin/preprocessor as < language-models/raw/as/wiki.txt > language-models/pre-processed/as/wiki.txt &
+./bin/preprocessor ca < language-models/raw/ca/wiki.txt > language-models/pre-processed/ca/wiki.txt &
+./bin/preprocessor cs < language-models/raw/cs/wiki.txt > language-models/pre-processed/cs/wiki.txt &
+./bin/preprocessor cy < language-models/raw/cy/wiki.txt > language-models/pre-processed/cy/wiki.txt &
+./bin/preprocessor de < language-models/raw/de/wiki.txt > language-models/pre-processed/de/wiki.txt &
+./bin/preprocessor en < language-models/raw/en/wiki.txt > language-models/pre-processed/en/wiki.txt &
+./bin/preprocessor en < language-models/raw/en-gb/wiki.txt > language-models/pre-processed/en-gb/wiki.txt &
+./bin/preprocessor en < language-models/raw/en-za/wiki.txt > language-models/pre-processed/en-za/wiki.txt &
+./bin/preprocessor es < language-models/raw/es/wiki.txt > language-models/pre-processed/es/wiki.txt &
+./bin/preprocessor es < language-models/raw/es-ar/wiki.txt > language-models/pre-processed/es-ar/wiki.txt &
+./bin/preprocessor es < language-models/raw/es-cl/wiki.txt > language-models/pre-processed/es-cl/wiki.txt &
+./bin/preprocessor es < language-models/raw/es-es/wiki.txt > language-models/pre-processed/es-es/wiki.txt &
+./bin/preprocessor es < language-models/raw/es-mx/wiki.txt > language-models/pre-processed/es-mx/wiki.txt &
+./bin/preprocessor fr < language-models/raw/fr/wiki.txt > language-models/pre-processed/fr/wiki.txt &
+./bin/preprocessor gl < language-models/raw/gl/wiki.txt > language-models/pre-processed/gl/wiki.txt &
+./bin/preprocessor hr < language-models/raw/hr/wiki.txt > language-models/pre-processed/hr/wiki.txt &
+./bin/preprocessor it < language-models/raw/it/wiki.txt > language-models/pre-processed/it/wiki.txt &
+./bin/preprocessor nb < language-models/raw/nb-no/wiki.txt > language-models/pre-processed/nb-no/wiki.txt &
+./bin/preprocessor pt < language-models/raw/pt-br/wiki.txt > language-models/pre-processed/pt-br/wiki.txt &
+./bin/preprocessor pt < language-models/raw/pt-pt/wiki.txt > language-models/pre-processed/pt-pt/wiki.txt &
+./bin/preprocessor ru < language-models/raw/ru/wiki.txt > language-models/pre-processed/ru/wiki.txt &
+./bin/preprocessor sl < language-models/raw/sl/wiki.txt > language-models/pre-processed/sl/wiki.txt &
